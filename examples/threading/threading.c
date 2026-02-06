1ea6474130bda563a7b5511c33582b0fe7346c02
dhmo9873 <dhmo9873@colorado.edu>
2026-02-05 23:03:11 -0700

    Fixed Error Handling, Unit Test pass even without waiting or delay as per requirement

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -15,6 +15,7 @@ void* threadfunc(void* thread_param)
     // hint: use a cast like the one below to obtain thread arguments from your parameter
 	// USED Example from Chapter 7 , page 238 , mutex example
 	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
 	pthread_mutex_lock (thread_func_args->mutex);
 	thread_func_args->thread_complete_success = true;
 	pthread_mutex_unlock (thread_func_args->mutex);
@@ -38,9 +39,11 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
 	 int ret;
 
 	 struct thread_data *thread_info = malloc(sizeof(struct thread_data));
+	 printf("HERE1\n");
 	 if ( thread_info == NULL ){
 		 return false;
 	 }
+	 printf("HERE2\n");
 	 thread_info->mutex = mutex;
 	 thread_info->wait_to_obtain_ms = wait_to_obtain_ms;
 	 thread_info->wait_to_release_ms = wait_to_release_ms;
@@ -48,13 +51,14 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
 
      //Ref From Chapt , Page 229	
 	 ret = pthread_create(ref_thread, NULL, threadfunc, (void *)thread_info);
-     if (!ret) {
+	 printf("HERE3\n");
+     if (ret !=0 ) {
 		 free(thread_info);
 		 errno = ret;
 		 perror("pthread_create");
+	 printf("HERE4\n");
 		 return false;
 	 }
-
 	return true;
 }
 
