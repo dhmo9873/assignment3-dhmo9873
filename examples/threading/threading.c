d5eb7608a2d2673870845d9c858a93ab1c62db25
dhmo9873 <dhmo9873@colorado.edu>
2026-02-07 22:20:37 -0700

    CLean-Up check on mutex loc and unlock Remove unnecessary comments and pointer reference

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -15,14 +15,24 @@ void* threadfunc(void* thread_param)
     // hint: use a cast like the one below to obtain thread arguments from your parameter
 	// USED Example from Chapter 7 , page 238 , mutex example
 	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
 	/* wait before obtaining mutex */
     usleep(thread_func_args->wait_to_obtain_ms * 1000);
 
-	pthread_mutex_lock (thread_func_args->mutex);
-	thread_func_args->thread_complete_success = true;
+	if (pthread_mutex_lock (thread_func_args->mutex) != 0 ){
+		thread_func_args->thread_complete_success = false;
+	    return thread_param;
+	}
+
 	/* wait before releasing mutex */
     usleep(thread_func_args->wait_to_release_ms * 1000);
-	pthread_mutex_unlock (thread_func_args->mutex);
+	
+	if (pthread_mutex_unlock (thread_func_args->mutex) != 0 ){
+		 thread_func_args->thread_complete_success = false;
+		 return thread_param;
+	}
+	
+	thread_func_args->thread_complete_success = true;
     
 	return thread_param;
 }
@@ -38,31 +48,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-	 pthread_t *ref_thread = thread;
 
 	 int ret;
 
 	 struct thread_data *thread_info = malloc(sizeof(struct thread_data));
-	 printf("HERE1\n");
 	 if ( thread_info == NULL ){
 		 return false;
 	 }
-	 printf("HERE2\n");
 	 thread_info->mutex = mutex;
 	 thread_info->wait_to_obtain_ms = wait_to_obtain_ms;
 	 thread_info->wait_to_release_ms = wait_to_release_ms;
 	 thread_info->thread_complete_success = false;
 
      //Ref From Chapt , Page 229	
-	 ret = pthread_create(ref_thread, NULL, threadfunc, (void *)thread_info);
-	 printf("HERE3\n");
-     if (ret !=0 ) {
+	 ret = pthread_create(thread, NULL, threadfunc, (void *)thread_info);
+     
+	 if (ret !=0 ) {
 		 free(thread_info);
 		 errno = ret;
 		 perror("pthread_create");
-	 printf("HERE4\n");
 		 return false;
 	 }
+	
 	return true;
 }
-
